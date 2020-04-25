b6de9c7f9e6585a8c0c652cab37010a846657d86
luojh <623597383@qq.com>
2020-04-25 14:14:53 +0800

    训练

--- src/view/routes/index.js
+++ src/view/routes/index.js
@@ -1,4 +1,24 @@
 import React, { Component } from 'react'
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) {
+      return false
+  }
+  let res = 0;
+  while (x !== 0) {
+      res = res * 10 + x % 10
+      x = ~~(x / 10);
+  }
+  if (res == x) {
+      return true;
+  } else {
+      return false;
+  }
+};
+console.log(isPalindrome(121))
 
 export default class index extends Component {
   render() {
