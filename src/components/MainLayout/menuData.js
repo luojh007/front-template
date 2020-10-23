229873d7a4eb19c2e476cad96579bf7f0731128f
luojh <623597383@qq.com>
2020-10-23 17:49:30 +0800

    add: 新增ag-grid分支

--- src/components/MainLayout/menuData.js
+++ src/components/MainLayout/menuData.js
@@ -1,62 +1,7 @@
 export const menuData = [
   {
     name: '表单相关演示',
-    url: '/lTable.html',
-    key: '1',
-  },
-  {
-    name: '导入文件弹窗演示',
-    url: '/importModal.html',
-    key: '2',
-  },
-  {
-    name: '动态多项新增',
-    icon: 'plus',
-    key: '3',
-    children: [{
-      name: '基本弹窗',
-      url: '/multipleAdd/base.html',
-      key: '3-1',
-    }, {
-      name: '复杂一',
-      url: '/multipleAdd/complexOne.html',
-      key: '3-2'
-    }]
-  },
-  {
-    name: 'antd文件上传+七牛',
-    url: '/uploadMultiple.html',
-    key: '4',
-    icon: 'upload'
-  },
-  {
-    name: '图表（BizCharts）',
-    icon: 'area-chart',
-    key: '5',
-    children: [{
-      name: 'tabs切换基础图表',
-      url: '/chart/tabsBase.html',
-      key: '5-1',
-    }]
-  },
-  {
-    name: '树结构',
-    icon: 'tree',
-    key: '6',
-    children: [{
-      name: '基本树',
-      url: '/tree/one.html',
-      key: '6-1',
-    }]
-  },
-  {
-    name: '弹窗',
-    icon: 'modal',
-    key: '7',
-    children: [{
-      name: '基本弹窗',
-      url: '/modal/one.html',
-      key: '7-1',
-    }]
+    url: '/table.html',
+    key: '1'
   }
-]
+];
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import { Provider } from 'react-redux'
-import App from './view/app'
-import store from './store/index'
-class Main extends React.Component{
-  constructor(){
+import { Provider } from 'react-redux';
+import App from './view/app';
+import store from './store/index';
+class Main extends React.Component {
+  constructor() {
     super();
   }
-  render(){
-    return <Provider store={store}><App/></Provider>
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   }
 }
 
-ReactDOM.render(<Main/>,document.getElementById('app'));
+ReactDOM.render(<Main />, document.getElementById('app'));
