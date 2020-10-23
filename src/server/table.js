9b4b805d8be86e83f499f453f6857850c0c5385d
luojh <623597383@qq.com>
2020-10-23 17:35:29 +0800

    feat: 新增ag_grid table使用

--- src/server/table.js
+++ src/server/table.js
@@ -1,9 +1,28 @@
 import request from '../core/_utils/request';
 
+// 获取表格数据
 export const getTableData = params => {
   return request({
     method: 'GET',
-    url: 'http://172.16.0.61:7272/Acme/ehrNewborn/online/sample',
+    url: 'http://172.17.90.55/Acme/ehrNewborn/online/sample',
+    data: { ...params }
+  });
+};
+
+// 获取head头部
+export const getColunmData = params => {
+  return request({
+    method: 'GET',
+    url: 'http://172.17.90.55/Acme/cm/formlist',
+    data: { ...params }
+  });
+};
+
+// 获取额外的table数据
+export const getExtraTableData = params => {
+  return request({
+    method: 'GET',
+    url: 'http://172.17.90.55/Acme/iEhr/SampleData',
     data: { ...params }
   });
 };
--- src/view/routes/OptimizeTable/Index.js
+++ src/view/routes/OptimizeTable/Index.js
@@ -2,31 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import 'ag-grid-enterprise';
 import { Button } from 'antd';
-import { getTableData } from '../../../server/table';
+import { getTableData, getColunmData, getExtraTableData } from '../../../server/table';
+import { objToArr } from '../../../core/_utils/common';
 const OptimizeTable = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
-
+  const [selectRow, setSelectRow] = useState([]);
   const [rowData, setRowData] = useState([]);
-  useEffect(() => {
-    const params = {
-      mothername: '',
+  const [columnData, setColumnData] = useState([]);
+  function onGridReady(params) {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+    const query = {
+      mothername: 'auto',
       barcode: '',
       personsex: '',
       samplequality: '',
       areaid: '',
       hosid: '',
-      labresult: '',
+      labresult: '正常',
       intervaltype: 'labstring',
       sampletype: '',
-      processstate: '',
+      processstate: '99',
       datetype: 'birthday',
       date1: '',
       date2: '',
       mobile: '',
       cavstate: '',
-      labitemstype: '',
+      labitemstype: '1',
+      labitems: 'PHE',
       samplestate: '',
       bloodQuality: '',
       geneType: '',
@@ -34,22 +40,23 @@ const OptimizeTable = () => {
       filename: '',
       cmcode: 'sample-export',
       modifydetail: {
-        mothername: '',
+        mothername: 'auto',
         barcode: '',
         personsex: '',
         samplequality: '',
         areaid: '',
         hosid: '',
-        labresult: '',
+        labresult: '正常',
         intervaltype: 'labstring',
         sampletype: '',
-        processstate: '',
+        processstate: '99',
         datetype: 'birthday',
         date1: null,
         date2: null,
         mobile: '',
         cavstate: '',
-        labitemstype: '',
+        labitemstype: '1',
+        labitems: 'PHE',
         samplestate: '',
         bloodQuality: '',
         geneType: '',
@@ -63,49 +70,114 @@ const OptimizeTable = () => {
       pagesize: '500',
       page: '1'
     };
-    getTableData(params).then(res => {
-      console.log(res);
+    getTableData(query).then(res => {
+      if (res && res.Rows) {
+        for (let index = 0; index < res.Rows.length; index++) {
+          res.Rows[index].preExpand = '';
+        }
+        setRowData(res.Rows);
+      }
     });
-    setTimeout(() => {
-      setRowData([
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxter', price: 72000 }
-      ]);
-    }, 500);
-  }, []);
-  function onGridReady(params) {
-    setGridApi(params.api);
-    setGridColumnApi(params.columnApi);
+    const parmas = {
+      centerid: 'bb717701-10de-47ee-b49e-693261c6df22',
+      code: 'grid-samplesearch',
+      _: 1603335075889
+    };
+    getColunmData(parmas).then(res => {
+      if (res && res[0]) {
+        const data = objToArr(JSON.parse(res[0].custom || '{}') || {});
+        // 手动增加展开前缀
+        data.length && data.unshift({ id: 'preExpand', elementname: '' });
+        setColumnData(data);
+      }
+    });
   }
 
-  const onButtonClick = e => {
+  function onButtonClick(e) {
     const selectedNodes = gridApi.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
-    const selectedDataStringPresentation = selectedData
-      .map(node => node.make + ' ' + node.model + ' ' + node.price)
-      .join(', ');
-    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+    setSelectRow(selectedData);
+  }
+  function getExpandData(item) {
+    console.log(item);
+    const { data, successCallback } = item;
+    const params = {
+      param: { condition: { sampleid: data.sampleid } }
+    };
+    getExtraTableData(params).then(res => {
+      if (res && res.Rows) {
+        successCallback(res.Rows);
+      }
+    });
+  }
+  function isFirstColumn(params) {
+    var displayedColumns = params.columnApi.getAllDisplayedColumns();
+    var thisIsFirstColumn = displayedColumns[1] === params.column;
+    return thisIsFirstColumn;
+  }
+
+  const MyRenderer = props => {
+    return <div>{props.value}</div>;
   };
   return (
-    <div className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
+    <div className='ag-theme-alpine' style={{ height: 800 }}>
       <Button onClick={onButtonClick}>Get selected rows</Button>
       <AgGridReact
-        suppressExpandablePivotGroups
-        groupDefaultExpanded={1}
-        onExpandOrCollapseAll={e => console.log(e)}
+        masterDetail={true}
         onGridReady={onGridReady}
         rowSelection='multiple'
         rowData={rowData}
+        pagination={true}
+        paginationPageSize={500}
+        defaultColDef={{
+          headerCheckboxSelection: isFirstColumn,
+          checkboxSelection: isFirstColumn
+        }}
+        frameworkComponents={{
+          myRenderer: MyRenderer
+        }}
+        detailCellRendererParams={{
+          detailGridOptions: {
+            columnDefs: [
+              { field: 'labitem', headerName: '实验项目' },
+              { field: 'labdate', headerName: '实验日期' },
+              {
+                field: 'labvalue',
+                headerName: '实验值'
+              },
+              {
+                field: 'labresult',
+                headerName: '实验结论'
+              },
+              {
+                field: 'processstate',
+                headerName: '流程状态',
+                cellRenderer: params => {
+                  return params.value === 2 ? '待验收' : params.value === 99 ? '完成' : '111';
+                }
+              },
+              {
+                field: 'labadvice',
+                headerName: '实验医嘱'
+              }
+            ]
+          },
+          getDetailRowData: getExpandData
+        }}
       >
-        <AgGridColumn
-          sortable={true}
-          filter={true}
-          checkboxSelection={true}
-          field='make'
-        ></AgGridColumn>
-        <AgGridColumn sortable={true} filter={true} field='model'></AgGridColumn>
-        <AgGridColumn sortable={true} filter={true} field='price'></AgGridColumn>
+        {columnData.map(({ id, elementname }, inx) => {
+          return (
+            <AgGridColumn
+              cellRenderer={inx === 0 ? 'agGroupCellRenderer' : 'myRenderer'}
+              showRowGroup={true}
+              field={id}
+              headerName={elementname}
+              filter={id !== 'preExpand'}
+              width={id === 'preExpand' ? 60 : ''}
+              key={inx}
+            />
+          );
+        })}
       </AgGridReact>
     </div>
   );
